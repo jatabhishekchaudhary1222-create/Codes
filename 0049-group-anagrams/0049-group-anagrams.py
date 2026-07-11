3dcf74a9fe4452453240c6764eae408c5e3f648a
Abhishek Chaudhary <jatabhishekchaudhary1222@gmail.com>
2026-07-11 23:44:04 +0530

    Time: 19 ms (78.98%), Space: 15.9 MB (84.79%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        
+        d={}
+        for word in strs:
+            key="".join(sorted(word))
+            if key in d:
+                d[key].append(word)
+            else:
+                d[key]=[word]
+        return list(d.values())
