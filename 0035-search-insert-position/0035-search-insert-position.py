4319506e778943e26b310151d80d633995e76026
Abhishek Chaudhary <jatabhishekchaudhary1222@gmail.com>
2026-07-18 01:09:15 +0530

    Time: 0 ms (100%), Space: 19.8 MB (81.18%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,13 +1,8 @@
-class Solution(object):
-    def searchInsert(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
         if target not in nums:
             nums.append(target)
             nums.sort()
         return nums.index(target)
-            
+
         
